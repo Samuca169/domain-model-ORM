906ca78a42c8989ac3d45b74ef95d7a739a6b7d2
Samuca169 <samucabarr169@gmail.com>
2023-10-25 10:09:17 -0300

    Categoria

---
+++ src/main/java/com/samucabarr/DomainModelORM/entities/Categoria.java
@@ -0,0 +1,40 @@
+package com.samucabarr.DomainModelORM.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GeneratedColumn;
+
+@Entity
+@Table(name = "tb_Categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
