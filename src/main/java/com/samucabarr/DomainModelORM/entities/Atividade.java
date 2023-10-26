8dbd2d3cd16e1b823fe9c683a627d830907e8d48
Samuca169 <samucabarr169@gmail.com>
2023-10-26 18:03:06 -0300

    Bloco e insert Participantes

--- src/main/java/com/samucabarr/DomainModelORM/entities/Atividade.java
+++ src/main/java/com/samucabarr/DomainModelORM/entities/Atividade.java
@@ -2,7 +2,9 @@ package com.samucabarr.DomainModelORM.entities;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -26,6 +28,9 @@ public class Atividade {
             joinColumns = @JoinColumn(name = "atividade_id"),
             inverseJoinColumns = @JoinColumn(name = "participante_id"))
     private Set<Participante> participantes = new HashSet<>();
+
+    @OneToMany(mappedBy = "atividade")
+    private List<Bloco> blocos = new ArrayList<>();
 
     public Atividade() {
     }
@@ -80,5 +85,9 @@ public class Atividade {
 
     public Set<Participante> getParticipantes() {
         return participantes;
+    }
+
+    public List<Bloco> getBlocos() {
+        return blocos;
     }
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_participante (nome, email) VALUES('José Silva', 'jose@gmail.com');
+INSERT INTO tb_participante (nome, email) VALUES('Tiago Faria', 'tiago@gmail.com');
+INSERT INTO tb_participante (nome, email) VALUES('Maria do Rosário', 'maria@gmail.com');
+INSERT INTO tb_participante (nome, email) VALUES('Teresa Silva', 'teresa@gmail.com');
